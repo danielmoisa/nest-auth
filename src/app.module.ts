5a1095761c37d75f10888bd0390b8ba766f79418
Daniel Moisa <danimoisa93@gmail.com>
2023-01-30 15:43:57 +0200

    add jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -15,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     PrismaModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
